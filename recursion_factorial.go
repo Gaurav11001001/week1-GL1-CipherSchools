1276bc36e190c8817c354d32d92a8eff488ea6da
Gaurav Bhagasra <122735934+Gaurav11001001@users.noreply.github.com>
2023-01-27 20:11:38 +0530

    Add files via upload

---
+++ recursion_factorial.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(fact(3))
+}
+func fact(number int) int {
+
+	// base case
+	if number == 1 || number == 0 {
+		return 1
+	}
+	// corner case
+	if number < 0 {
+		fmt.Println("Invalid Number")
+	}
+	result := number * fact(number-1)
+	return result
+
+}
