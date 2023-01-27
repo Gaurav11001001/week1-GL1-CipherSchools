62318f7eca9bd65aaf1fee85e442f3c26d89c49f
Gaurav Bhagasra <122735934+Gaurav11001001@users.noreply.github.com>
2023-01-27 19:49:24 +0530

    Add files via upload

---
+++ loops.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			fmt.Println(i, j)
+		}
+	}
+
+	for i := 0; i < 3; i++ {
+		if i == 1 {
+			continue
+		}
+		fmt.Println(i)
+	}
+
+	nums := []int{1, 2, 3, 4, 5}
+	for k, l := range nums {
+		fmt.Println(k)
+		fmt.Println(l)
+	}
+}
